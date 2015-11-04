7883088151d5e53ed65131033a55aec9913581e6
andmilj <andrej.miljus@gmail.com>
2015-11-04 23:20:53 +0100

    MainPage with css

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,13 @@ import React from 'react';
 import Router from 'react-routing/src/Router';
 import http from './core/HttpClient';
 import App from './components/App';
-import ContentPage from './components/ContentPage';
+// import ContentPage from './components/ContentPage';
 import ContactPage from './components/ContactPage';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorPage from './components/ErrorPage';
+import MainPage from './components/MainPage';
 
 const router = new Router(on => {
   on('*', async (state, next) => {
@@ -26,9 +27,9 @@ const router = new Router(on => {
 
   on('/register', async () => <RegisterPage />);
 
-  on('*', async (state) => {
-    const content = await http.get(`/api/content?path=${state.path}`);
-    return content && <ContentPage {...content} />;
+  on('*', async () => {
+    // const content = await http.get(`/api/content?path=${state.path}`);
+    return <MainPage />;
   });
 
   on('error', (state, error) => state.statusCode === 404 ?
