050a9c9c66ec06f971ef0403182814a043bd5ec8
Matko Bulić <bulicmatko@gmail.com>
2015-11-21 15:03:54 +0100

    add user actions

---
+++ src/actions/UserActions.js
@@ -0,0 +1,16 @@
+
+const UserActions = {
+
+  authenticateUser (username, password) {
+    // @TODO
+    // Send request to the server
+    console.log(username, password);
+  },
+
+  unauthenticateUser () {
+    console.log('UNAUTHENTICATE!!!');
+  }
+
+};
+
+export default UserActions;
