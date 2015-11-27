214541c9d015a729751da2b353be2dd01c9ce334
Matko Bulić <bulicmatko@gmail.com>
2015-11-27 01:22:46 +0100

    add jQuery and post to api

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import App from './components/App';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorPage from './components/ErrorPage';
 import MainPage from './components/MainPage';
+import LoginPage from './components/LoginPage';
 
 const router = new Router(on => {
   on('*', async (state, next) => {
@@ -14,7 +15,7 @@ const router = new Router(on => {
     return component && <App context={state.context}>{component}</App>;
   });
 
-  // on('/login', async () => <LoginPage />);
+  on('/login', async () => <LoginPage />);
 
   on('*', async () => {
     const destinations = await http.get('/api/destinations');
