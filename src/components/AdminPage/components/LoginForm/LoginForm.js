f25e76505eb2b543074b1b68c30caef3fc2e5259
Matko Bulic <bulicmatko@gmail.com>
2016-01-14 21:24:41 +0100

    adjust login form

--- src/components/AdminPage/components/LoginForm/LoginForm.js
+++ src/components/AdminPage/components/LoginForm/LoginForm.js
@@ -2,11 +2,8 @@
 
 import React, { PropTypes, Component } from 'react';
 import _ from 'lodash';
-import $ from 'jquery';
 
 import withStyles from '../../../../decorators/withStyles';
-
-import Input from '../../../ToolBox/Input';
 
 import styles from './LoginForm.scss';
 
@@ -54,49 +51,70 @@ class LoginForm extends Component {
       });
   }
 
-  _getInputError(inputName) {
+  _getError(inputName) {
     const { errors } = this.state;
 
     return _.result(_.find(errors, { field: inputName }), 'message');
   }
 
-  _renderInputUsername() {
+  _renderInput(config) {
+    const { label, type, placeholder, value, onChange, error } = config;
+    const inputId = _.uniqueId();
+
     return (
-      <Input
-        label="Username"
-        type="text"
-        placeholder="Username"
-        value={this.state.username}
-        onChange={(e) => this._handleInputChange('username', e.target.value)}
-        error={this._getInputError('username')}
-      />
+      <div className="f-row">
+        <div className="full-width">
+          <label htmlFor={inputId}>{label}</label>
+          {error && <span className="input-error">{error}</span>}
+          <input
+            id={inputId}
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+          />
+        </div>
+      </div>
     );
+  }
+
+  _renderInputUsername() {
+    return this._renderInput({
+      label: 'Username',
+      type: 'text',
+      placeholder: 'Username',
+      value: this.state.username,
+      onChange: (e) => this._handleInputChange('username', e.target.value),
+      error: this._getError('username'),
+    });
   }
 
   _renderInputPassword() {
-    return (
-      <Input
-        label="Password"
-        type="password"
-        placeholder="Password"
-        value={this.state.password}
-        onChange={(e) => this._handleInputChange('password', e.target.value)}
-        error={this._getInputError('password')}
-      />
-    );
+    return this._renderInput({
+      label: 'Password',
+      type: 'password',
+      placeholder: 'Password',
+      value: this.state.password,
+      onChange: (e) => this._handleInputChange('password', e.target.value),
+      error: this._getError('password'),
+    });
   }
 
   _renderAuthError() {
-    const error = this._getInputError('*');
+    const error = this._getError('*');
 
     return error
-      ? <div className="error-message alert alert-danger text-center">{error}</div>
+      ? <div className="f-row auth-error">{error}</div>
       : null;
   }
 
   _renderSubmitButton() {
     return (
-      <button type="submit" className="btn btn-border">Login</button>
+      <div className="f-row">
+        <div className="full-width">
+          <input type="submit" value="Login" className="btn color medium full" />
+        </div>
+      </div>
     );
   }
 
@@ -105,22 +123,22 @@ class LoginForm extends Component {
     this.context.onSetTitle(title);
 
     return (
-      <form className="LoginForm" onSubmit={this._handleSubmit.bind(this)}>
-        {this.state.user
-          ? this._renderLoginSuccess()
-          : (
-              <div>
-                <h1>{title}</h1>
-                <br/>
-                {this._renderAuthError()}
-                {this._renderInputUsername()}
-                {this._renderInputPassword()}
-                <br/>
-                {this._renderSubmitButton()}
+      <main className="main" role="main">
+        <div className="wrap">
+          <div className="row">
+            <div className="content one-half modal">
+              <div className="box">
+                <form className="LoginForm" onSubmit={this._handleSubmit.bind(this)}>
+                  {this._renderInputUsername()}
+                  {this._renderInputPassword()}
+                  {this._renderAuthError()}
+                  {this._renderSubmitButton()}
+                </form>
               </div>
-            )
-        }
-      </form>
+            </div>
+          </div>
+        </div>
+      </main>
     );
   }
 
