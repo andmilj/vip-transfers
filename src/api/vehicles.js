aee0ebc3cd3dae3cb6e4c75ee0e4871309911fce
Andrej Miljus <andrej.miljus@gmail.com>
2016-01-19 21:44:20 +0100

    added vehicle infos to MainPage.js

---
+++ src/api/vehicles.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import vehicle from '../db/models/vehicle';
+
+const router = new Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    vehicle.find({}, (err, vehicles) => {
+      if (err) {
+        next(err);
+      }
+
+      res.status(200).json(vehicles);
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
