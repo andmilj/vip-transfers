f47d560ea54f2520185a520bffb354d503420c0e
Andrej Miljus <andrej.miljus@infobip.com>
2016-02-01 23:50:07 +0100

    reservation success text

--- src/components/ResultsPage/Success.react.js
+++ src/components/ResultsPage/Success.react.js
@@ -24,19 +24,8 @@ class Success extends Component {
               <div className="content">
                 <h2>Your reservation was successfull!</h2>
                 <p className="lead">
-                  Far far away, behind the word mountains,
-                  far from the countries Vokalia and Consonantia,
-                  there live the blind texts. Separated they live in Bookmarksgrove
-                  right at the coast of the Semantics, a large language ocean. A small
-                  river named Duden flows by their place and supplies it with the necessary
-                  regelialia.
-                </p>
-                <p>
-                  It is a paradisematic country, in which roasted parts of sentences
-                  fly into your mouth. Even the all-powerful Pointing has no control about
-                  the blind texts it is an almost unorthographic life One day however
-                  a small line of blind text by the name of Lorem Ipsum decided to leave
-                  for the far World of Grammar.
+                  Thank you for choosing us. You will receive SMS 24h before your
+                  journey as remark. If you have any further questions, please contact us.
                 </p>
               </div>
             </div>
